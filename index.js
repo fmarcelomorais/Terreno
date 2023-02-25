d949e614441760c56bf03d6c83d64bde21c32ef4
fmarcelomorais <marcelomorais.program@gmail.com>
2023-02-25 00:36:18 -0300

    altera

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-//const server = require('./src/server/server');
-const server = require('./src/app/app')
-
-
-const PORT = process.env.PORT || 5001;
-server.listen(PORT, () => console.log(`Conectado na porta ${PORT}`))
-
-//module.exports = server;
+const server = require('./src/server/server');
