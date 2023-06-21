9dd3ffe2e3bd7a239e60fa01ce5ce176897450c5
Marcelo Morais <81336017+fmarcelomorais@users.noreply.github.com>
2023-06-20 23:06:15 -0300

    Update constants.js

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,7 +1,7 @@
-const PARTIALS_DIR = 'Terreno/src/views/partials';
-const LAYOUTS_DIR = 'Terreno/src/views/layouts';
-const VIEWS_DIR = 'Terreno/src/views';
-const MAIN_DIR = 'Terreno/src/views/layouts/main.hbs';
+const PARTIALS_DIR = 'terreno-jyf7sep1f-fmarcelomorais.vercel.app/Terreno/src/views/partials';
+const LAYOUTS_DIR = 'terreno-jyf7sep1f-fmarcelomorais.vercel.app/Terreno/src/views/layouts';
+const VIEWS_DIR = 'terreno-jyf7sep1f-fmarcelomorais.vercel.app/Terreno/src/views';
+const MAIN_DIR = 'terreno-jyf7sep1f-fmarcelomorais.vercel.app/Terreno/src/views/layouts/main.hbs';
 
 module.exports = {
     PARTIALS_DIR,
