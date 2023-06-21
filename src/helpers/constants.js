338d628f0f88706ebd069c23b8f97dacd5bf3c34
Marcelo Morais <81336017+fmarcelomorais@users.noreply.github.com>
2023-06-20 23:04:30 -0300

    Update constants.js

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,7 +1,7 @@
 const PARTIALS_DIR = 'Terreno/src/views/partials';
-const LAYOUTS_DIR = 'Terreno//src/views/layouts';
-const VIEWS_DIR = 'Terreno//src/views';
-const MAIN_DIR = 'main.hbs';
+const LAYOUTS_DIR = 'Terreno/src/views/layouts';
+const VIEWS_DIR = 'Terreno/src/views';
+const MAIN_DIR = 'Terreno/src/views/layouts/main.hbs';
 
 module.exports = {
     PARTIALS_DIR,
