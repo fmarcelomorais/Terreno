9745b4b5dcd3abe2f255bbf55d3ace3c4d3c7b35
fmarcelomorais <marcelomorais.program@gmail.com>
2023-12-25 19:50:33 -0300

    altera strictQuery mongose de posição

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -7,8 +7,8 @@ const banco = process.env.DB_BANCO
 const conector = `mongodb+srv://${user}:${password}@${banco}/?retryWrites=true&w=majority`
 
 async function conect(){
-    await mongoose.connect(conector) 
     mongoose.set('strictQuery', false);   
+    await mongoose.connect(conector) 
     console.log('Conectado ao banco de dados')
 
 }
