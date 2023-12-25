c78a3c401fcb71101fea02686909849e4166f8fd
fmarcelomorais <marcelomorais.program@gmail.com>
2023-12-25 20:03:47 -0300

    mongoose

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -7,7 +7,7 @@ const banco = process.env.DB_BANCO
 const conector = `mongodb+srv://${user}:${password}@${banco}/?retryWrites=true&w=majority`
 
 async function conect(){
-    mongoose.set('strictQuery', true);   
+    mongoose.set('strictQuery', false);   
     await mongoose.connect(conector) 
     console.log('Conectado ao banco de dados')
 
